70cfb917b4c5b18dd06e3725cbbfe920133583b3
XING YAHAO <keidarcy.1015@gmail.com>
2021-05-09 12:27:24 +0900

    updated bg

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       sudoku: CreateValidSudoku()
     }));
     this.btnRef = React.createRef();
+    this.bgRef = React.createRef();
   }
 
   handleChange = (e) => {
@@ -58,7 +59,32 @@ class App extends Component {
 
     dfs(board);
 
+    // hsl(228deg 100% 70%), hsl(150deg 100% 80%), hsl(168deg 100% 50%), hsl(195deg 100% 60%), hsl(120deg 100% 80%), hsl(140deg 100% 70%), hsl(180deg 100% 50%)
+    const genColor = () => {
+      const hue = (Math.random() * 300) | 0;
+      const sign = Math.random() * 2 > 1 ? 1 : -1;
+
+      const genOne = () => {
+        const oneHue = hue * ((sign * Math.random()) / 1.5 + 1);
+        const lightness = (100 - Math.random() * 30) | 0;
+        return `hsl(${oneHue}deg 100% ${lightness}%)`;
+      };
+      let res = '';
+      for (let i = 0; i < 7; i++) {
+        res += genOne();
+        if (i < 6) {
+          res += ',';
+        }
+      }
+      return res;
+    };
     this.btnRef.current.style.setProperty('--tesla-blur', Math.random() * 100);
+    this.bgRef.current.style.setProperty('--voronoi-cell-colors', genColor());
+    this.bgRef.current.style.setProperty(
+      '--voronoi-number-of-cells',
+      (30 - Math.random() * 20) | 0
+    );
+
     this.setState(
       produce((state) => {
         for (let row = 0; row < 9; row++) {
@@ -76,7 +102,7 @@ class App extends Component {
         <header className="App-header static space">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <div className="main">
+        <div ref={this.bgRef} className="main">
           <SudokuBoard sudoku={this.state.sudoku} onChange={this.handleChange} />
           <button ref={this.btnRef} onClick={this.solveSudoku} className="tesla solve">
             solve
--- src/components/SudokuBoard.jsx
+++ src/components/SudokuBoard.jsx
@@ -3,18 +3,16 @@ import { SudokuField } from './SudokuField'
 
 const SudokuBoard = ({sudoku, onChange}) => {
   return (
-    <>
-      <pre>
-        {sudoku.rows.map(row => (
-          <div key={row.index + '+row'}>
-            {row.cols.map(field => <SudokuField key={`${row.index}+${field.col}`} field={field} onChange={onChange} />)}
+      <div className="game">
+        <div className="board">
+          {sudoku.rows.map(row => (
+            <div key={row.index + '+row'}>
+              {row.cols.map(field => <SudokuField key={`${row.index}+${field.col}`} field={field} onChange={onChange} />)}
+          </div>
+        ))}
         </div>
-      ))}
-      </pre>
-    </>
-
+      </div>
   )
-  
 }
 
 export default SudokuBoard
